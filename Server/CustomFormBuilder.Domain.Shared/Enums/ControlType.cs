60d155e5fd2237a81e44f5dd0be9d901e1ab9317
Nooshin Shokrollahi <nshokrollahi951@gmail.com>
2025-01-22 17:06:09 +0100

    Edit ControlType enum

---
+++ Server/CustomFormBuilder.Domain.Shared/Enums/ControlType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomFormBuilder.Domain.Shared.Enums
+{
+    public enum ControlType
+    {
+        TextBox,        
+        TextArea,       
+        Password,       
+        Email,          
+        Number,       
+        Dropdown,       
+        MultiSelect,   
+        RadioButton,    
+        CheckBox, 
+        Date,           
+        Time,           
+        DateTime,
+        Button,   
+        Label   
+    }
+}
