39a374bd9dda1e7d8f5e8356dae16024992eb2c7
Nooshin Shokrollahi <nshokrollahi951@gmail.com>
2025-02-02 12:57:25 +0100

    Repo Interfaces are added

---
+++ Server/CustomFormBuilder.Domain/FormSubmissions/IFormSubmissionRepository.cs
@@ -0,0 +1,13 @@
+﻿using CustomFormBuilder.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomFormBuilder.Domain.FormSubmissions
+{
+    public interface IFormSubmissionRepository:IBaseRepository<FormSubmission, Guid>
+    {
+    }
+}
---
+++ Server/CustomFormBuilder.Domain/FormVersions/IFormVersionRepository.cs
@@ -0,0 +1,13 @@
+﻿using CustomFormBuilder.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomFormBuilder.Domain.FormVersions
+{
+    public interface IFormVersionRepository:IBaseRepository<FormVersion, Guid>
+    {
+    }
+}
